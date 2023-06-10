0d52a25b241fc2b0ad477e57d6955c337a5786bb
simplystudios <new.anshwadhwa@gmail.com>
2023-06-10 19:52:14 +0530

    new ui and api

--- main/app.js
+++ main/app.js
@@ -5,6 +5,7 @@ const searchInput = document.getElementById("search-input");
 const Imagecontain = document.getElementById("img")
 const Pagetitle = document.getElementById("pgt");
 const Ogdesc = document.getElementById("desc")
+const Background = document.getElementById("result")
 
 const searchin = () => {
   const searchText = searchInput.value;
@@ -15,24 +16,26 @@ const searchin = () => {
 };
 
 const search = window.location.search.substr(1);
-const apiurl = `https://anshwadhwa.pythonanywhere.com/api/search/hinduism-${search}`;
+const apiurl = `https://en.wikipedia.org/w/rest.php/v1/search/page?q=hinduism-${search}&limit=3`;
 let Descriptionex;
 let Titleex
 let Imageex
 const performSearch = () => {
   if (search) { // Check if search variable is not empty
-    const cacheKey = `hinduwiki_cache_${search}`;
+    const cacheKey = `hinduwiki_cache_${search}_${Date.now}`;
     const cachedData = localStorage.getItem(cacheKey);
 
     if (cachedData) {
-      const { extract, searchtitle, imgrc } = JSON.parse(cachedData);
-      Container.textContent = extract;
-      Titlecontain.textContent = searchtitle;
-      Imagecontain.src = imgrc;
-      Ogdesc.textContent = extract;
-      Descriptionex = extract;
-      Titleex = searchtitle;
-      Imageex = imgrc;
+      const { excerpt, title, thumbnail } = JSON.parse(cachedData).pages[0];
+      const sanitizedExcerpt = excerpt.replace(/<\/?span[^>]*>/g, ""); // Remove <span> tags from excerpt
+      const firstThumbnail = thumbnail.url.replace(/\/\d+px-/g, "/1200px-"); // Update thumbnail quality to 200px // Get the first thumbnail URL
+      Container.textContent = sanitizedExcerpt;
+      document.body.style.backgroundImage = `url('${firstThumbnail}')`;
+      Titlecontain.textContent = title;
+      Ogdesc.textContent = sanitizedExcerpt;
+      Descriptionex = sanitizedExcerpt;
+      Titleex = title;
+      Imageex = thumbnail.url;
     } else {
       Pagetitle.textContent = `Hinduwiki • ${search}`; // Set the page title
       fetch(apiurl)
@@ -42,17 +45,20 @@ const performSearch = () => {
             Container.textContent = response.error;
             Ogdesc.textContent = response.error;
           } else {
-            const { description: extract, title: searchtitle, image_url: imgrc } = response;
-            Container.textContent = extract;
-            Titlecontain.textContent = searchtitle;
-            Imagecontain.src = imgrc;
-            Ogdesc.textContent = extract;
-            Descriptionex = extract;
-            Titleex = searchtitle;
-            Imageex = imgrc;
+            const { excerpt, title, thumbnail } = response.pages[0];
+            const sanitizedExcerpt = excerpt.replace(/<\/?span[^>]*>/g, ""); // Remove <span> tags from excerpt
+            const firstThumbnail = thumbnail.url.replace(/\/\d+px-/g, "/1200px-"); // Update thumbnail quality to 200px // Get the first thumbnail URL
+            Container.textContent = sanitizedExcerpt;
+            Titlecontain.textContent = title;
+            document.body.style.backgroundImage = `url('${firstThumbnail}')`;
+            Ogdesc.textContent = sanitizedExcerpt;
+            Descriptionex = sanitizedExcerpt;
+            Titleex = title;
+            Imageex = thumbnail.url;
+
 
             // Cache the data
-            const dataToCache = { extract, searchtitle, imgrc };
+            const dataToCache = { pages: [{ excerpt: sanitizedExcerpt, title, thumbnail }] };
             localStorage.setItem(cacheKey, JSON.stringify(dataToCache));
           }
         })
@@ -66,9 +72,11 @@ const performSearch = () => {
   } else {
     Titlecontain.textContent = "Error Code: 1";
     Container.textContent = "Please Specify A Search Term In The URL In This Format: hinduwiki.ml/main?{your search term}";
-    Ogdesc.textContent = "Please Specify A Search Term In The URL In This Format: hinduwiki.ml/main?{your search term}"
+    Ogdesc.textContent = "Please Specify A Search Term In The URL In This Format: hinduwiki.ml/main?{your search term}";
   }
 };
+
+
 
 performSearch();
 
